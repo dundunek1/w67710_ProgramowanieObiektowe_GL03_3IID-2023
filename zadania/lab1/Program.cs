9f3b443f9e9d5e2bfff53addbeaef540c7a8537d
dundunek1 <115711835+dundunek1@users.noreply.github.com>
2023-10-25 13:56:10 +0200

    Add files via upload

---
+++ zadania/lab1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+
+namespace lab1;
+
+class lab1
+{
+    static void Main(string[] args)
+    {
+        /*
+        //Zadanie1
+        Console.WriteLine("podaj liczbe:");
+        var number2 = Console.ReadLine();
+        Console.WriteLine(number2);  
+        var number =int.Parse(number2);
+        if(number % 2 ==0)
+        {
+            Console.WriteLine("parzysta");
+        }
+        else
+        {
+            Console.WriteLine("Nieparzysta");
+        }*/
+        //Menu
+        /*Console.WriteLine("Wybierz funkcje");
+        Console.WriteLine(" 1 - Funkcja1");
+        Console.WriteLine(" 2 - Funkcja2");
+        Console.WriteLine(" 3 - Funkcja3");
+        var input = Console.ReadLine();
+        switch (int.Parse(input))
+        {
+            case 1:
+                Console.WriteLine("Wybrales funkcje 1");
+                break;
+            case 2:
+                Console.WriteLine("Wybrales funkcje 2");
+                break;
+            case 3:
+                Console.WriteLine("Wybrales funckje 3");
+                break;
+        }*/
+
+    }
+
+}
